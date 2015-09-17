d08af671a6a423262316441a1e2207a4ba3e33d2
Kaseyann7 <Kaseyann17@gmail.com>
2015-09-17 11:18:12 -0400

    Burma shave.cs

--- chapter1/BurmaShave/Program.cs
+++ chapter1/BurmaShave/Program.cs
@@ -9,6 +9,12 @@ namespace BurmaShave
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("The Queen");
+            Console.WriteLine("of hearts");
+            Console.WriteLine("Now loves the knave");
+            Console.WriteLine("The King");
+            Console.WriteLine("Ran out of");
+            Console.WriteLine("Burma Shave");
         }
     }
 }
